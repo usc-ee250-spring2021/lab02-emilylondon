e5e9d274443a35023b40a15ffbcc277eeb35ef23
Emily London <elondon@usc.edu>
2021-02-02 10:26:42 -0500

    Test the LCD Screen

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -1,7 +1,7 @@
 """ EE 250L Lab 02: GrovePi Sensors
 
 List team members here.
-
+Emily London
 Insert Github repository link here.
 """
 
@@ -23,6 +23,7 @@ sys.path.append('../../Software/Python/')
 sys.path.append('../../Software/Python/grove_rgb_lcd')
 
 import grovepi
+from grove_rgb_lcd import *
 
 """This if-statement checks if you are running this python file directly. That 
 is, if you run `python3 grovepi_sensors.py` in terminal, this if-statement will 
@@ -30,6 +31,9 @@ be true"""
 if __name__ == '__main__':
     PORT = 4    # D4
 
+    setText("Hello world! test")
+    setRGB(0,128,64)
+    
     while True:
         #So we do not poll the sensors too quickly which may introduce noise,
         #sleep for a reasonable time of 200ms between each iteration.
